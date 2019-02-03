6d62ba1cd106761c9fd40b73cdba2496a27d4aac
milselarch <milselarch@gmail.com>
2019-02-03 13:01:04 +0800

    initial commit

---
+++ NodeMCU/smart_aircon_esp/smart_aircon_esp.ino
@@ -0,0 +1,46 @@
+#include "HVAC_LIB_ESP8266.h";
+#include <ESP8266WebServer.h>    // Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>         // https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+WiFiManager wifiManager;
+ESP8266WebServer server(80);
+HvacMitsubishi AirController((unsigned int) D5);
+boolean sendMessage = false;
+
+void handle_OnConnect () {
+    Serial.print("ON-CONNECT");
+    String page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
+    sendMessage = true;
+    server.send(200, "text/html", page);
+}
+
+void setup () {
+    Serial.begin(115200);
+
+    Serial.print("Connecting Wifi: ");
+    wifiManager.autoConnect("AutoConnectAP");
+
+    delay(1000);
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    IPAddress ip = WiFi.localIP();
+    Serial.println(ip);
+
+    Serial.println("AIRCON TEST START");
+
+    server.on("/", handle_OnConnect);
+    server.begin();
+}
+
+void loop () {
+    server.handleClient();
+
+    if (sendMessage) {
+        Serial.print("SEND-IR: ");
+        Serial.println(millis());
+        AirController.sendHvac(HVAC_AUTO, 25, FAN_SPEED_AUTO, VANNE_AUTO_MOVE, true);
+        sendMessage = false;
+    }
+}
---
+++ app/app/components/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to NativeScript-Vue!"/>
+
+        <GridLayout columns="*" rows="100, 100, 100" width="300" height="600" backgroundColor="lightgray" >
+          <Label class="message" :text="msg" col="0" row="0"/>
+          <TextField class="message" col="0" row="1" v-model="inputTemp" />
+          <Label class="message" :text="inputTemp" col="0" row="2"/>
+          <Button text="Button" @tap="add" />
+        </GridLayout>
+    </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!',
+        inputTemp: 22
+      }
+    },
+
+    methods: {
+      add () {
+        this.inputTemp += 1
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  ActionBar {
+    background-color: #53ba82;
+    color: #ffffff;
+  }
+
+  .message {
+    vertical-align: center;
+    text-align: center;
+    font-size: 20;
+    color: #333333;
+  }
+</style>
