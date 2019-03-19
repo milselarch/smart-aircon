df02c37ecbe1a31466362793df4ba6e12a0c64b1
milselarch <milselarch@gmail.com>
2019-03-19 21:25:26 +0800

    add method to output IR sequence given temp, fan speed, and on / off state

--- NodeMCU/smart-aircon/smart-aircon.ino
+++ NodeMCU/smart-aircon/smart-aircon.ino
@@ -1,21 +1,36 @@
-# include "sender.h"
+#include <ESP8266WebServer.h>
+#include <AutoConnect.h>
+#include "sender.h"
 
+ESP8266WebServer WebServer;
+AutoConnect Portal(WebServer)
 PulseSender sender(D1);
+
+void homePage() {
+    WebServer.send(200, "text/plain", "Hello WOrld Owo");
+}
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
     Serial.println("TESTING1");
     Serial.println("TESTING2");
+
+    Server.on("/", homePage);
+    Portal.begin();
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
-    
+
+    Portal.handleClient();
+
+    /*
     if (Serial.available()) {
         String line = Serial.readString();
         Serial.print("SENDING ");
         Serial.println(line);
         sender.sendSequence(line);
     }
+    */
 }
